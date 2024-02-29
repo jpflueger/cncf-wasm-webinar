13340db7e21649e0e78f60cefae9a49621736073
Justin Pflueger <justin.pflueger@fermyon.com>
2024-02-29 06:20:51 -0700

    fix ingress application

--- infra/civo/main.tf
+++ infra/civo/main.tf
@@ -8,13 +8,53 @@ resource "civo_firewall" "main" {
   name       = var.name
   network_id = civo_network.main.id
 
-  create_default_rules = true
+  create_default_rules = false
 
   ingress_rule {
+    label      = "6443 port(s) open"
     action     = "allow"
+    protocol   = "tcp"
     cidr       = var.allowed_api_cidrs
-    label      = "kubernetes-api-server"
     port_range = "6443"
+  }
+
+  ingress_rule {
+    label      = "80 port(s) open"
+    action     = "allow"
+    protocol   = "tcp"
+    cidr       = ["0.0.0.0/0"]
+    port_range = "80"
+  }
+
+  ingress_rule {
+    label      = "443 port(s) open"
+    action     = "allow"
+    protocol   = "tcp"
+    cidr       = ["0.0.0.0/0"]
+    port_range = "443"
+  }
+
+  egress_rule {
+    label      = "All TCP ports open"
+    action     = "allow"
+    protocol   = "tcp"
+    cidr       = ["0.0.0.0/0"]
+    port_range = "0-65535"
+  }
+
+  egress_rule {
+    label      = "All UDP ports open"
+    action     = "allow"
+    protocol   = "udp"
+    cidr       = ["0.0.0.0/0"]
+    port_range = "0-65535"
+  }
+
+  egress_rule {
+    label    = "Ping/traceroute"
+    action   = "allow"
+    protocol = "icmp"
+    cidr     = ["0.0.0.0/0"]
   }
 }
 
@@ -22,6 +62,10 @@ resource "civo_firewall" "main" {
 resource "civo_kubernetes_cluster" "main" {
   name         = var.name
   cluster_type = "k3s"
+
+  kubernetes_version = "1.27.1-k3s1"
+
+  applications = "traefik2-loadbalancer,metrics-server"
 
   cni         = var.cni
   network_id  = civo_network.main.id
@@ -32,4 +76,9 @@ resource "civo_kubernetes_cluster" "main" {
     size       = var.node_size
     node_count = var.node_count
   }
+}
+
+# find the cluster's load balancer
+data civo_loadbalancer "main" {
+  name = "${var.name}-kube-system-traefik"
 }
--- infra/civo/outputs.tf
+++ infra/civo/outputs.tf
@@ -3,3 +3,11 @@ output "kubeconfig" {
   description = "kubeconfig for created cluster"
   sensitive   = true
 }
+
+output "cluster_domain" {
+  value = civo_kubernetes_cluster.main.dns_entry
+}
+
+output "lb_domain" {
+  value = "${data.civo_loadbalancer.main.id}.lb.civo.com"
+}
